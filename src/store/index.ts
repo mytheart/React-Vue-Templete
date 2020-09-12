507a23e29b6b5a568c0c6ff2fe6fa0294d7b64f7
zhangYin <wb-zy584494@alibaba-inc.com>
2020-09-12 23:44:28 +0800

    ref:设置store的自动导入

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
+
+interface objConfig {
+  [key: string]: any;
+}
+
+const modules: objConfig = {};
+const require_module = require.context('./modules', false, /.ts$/);
+require_module.keys().forEach(file_name => {
+  modules[file_name.slice(2, -3)] = require_module(file_name).default;
+});
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules
 });
