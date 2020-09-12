5d83c5bd99b868d814736f58e43b84b431eb09b0
mytheart <550703617@qq.com>
2020-09-12 16:34:20 +0800

    delete:删除vue2项目的全部配置

--- src/http/api.js
+++
@@ -1,6 +0,0 @@
-// 接口文档地址： https://cn.vuejs.org/index.html
-
-/**
- * 获取管理员信息
- */
-export const getManagerInfo = '/api/getUserInfo';
--- src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export * from './common';
-export * from './md5';
-export * from './amap';
-export * from './zlb';
