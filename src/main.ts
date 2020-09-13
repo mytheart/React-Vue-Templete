a1e6cf0c6e08d7c77412547dd28eabb372a8de66
“mytheart” <550703617@qq.com>
2020-09-13 10:42:00 +0800

    feat:新增组件

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import { Button, Loading } from 'vant';
+import { Button, Loading , DropdownMenu, DropdownItem, ShareSheet, Cell} from 'vant';
 
 const app = createApp(App);
 
 app
   .use(Button)
+  .use(ShareSheet)
+  .use(Cell)
   .use(Loading)
+  .use(DropdownMenu)
+  .use(DropdownItem)
   .use(store)
   .use(router)
   .mount('#app');
