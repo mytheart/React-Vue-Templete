9c3a1d8ce2b547a71fefe2e633f587ffc15f8f4f
“mytheart” <550703617@qq.com>
2020-09-13 10:42:40 +0800

    ref:完善app页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
+<van-cell title="显示分享面板" @click="showShare = true" />
+
+  <van-share-sheet
+    v-model:show="showShare"
+    title="立即分享给好友"
+    :options="options"
+  />
+
   <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showShare: false,
+      options: [
+        [
+          { name: '微信', icon: 'wechat' },
+          { name: '微博', icon: 'weibo' },
+          { name: 'QQ', icon: 'qq' }
+        ],
+        [
+          { name: '复制链接', icon: 'link' },
+          { name: '分享海报', icon: 'poster' },
+          { name: '二维码', icon: 'qrcode' }
+        ]
+      ]
+    };
+  }
+};
+</script>
 
 <style lang="less">
 @import './assets/less/reset.less';
