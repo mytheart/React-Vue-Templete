b49330d74cc285828955871cfd8c1505b9b36ad1
“mytheart” <550703617@qq.com>
2020-09-14 15:17:07 +0800

    ref:新增about页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '../views/Home';
+import About from '../views/About';
 
 Vue.use(VueRouter);
 
@@ -12,6 +13,14 @@ const routes = [
     component: Home,
     meta: {
       title: '首页'
+    }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+    meta: {
+      title: 'About'
     }
   }
 ];
---
+++ src/views/About/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+
+  </div>
+</template>
+
+<script>
+// import { reactive, ref } from 'vue';
+
+// export default {
+//   name: 'About',
+
+//   setup() {
+//     const count = ref(0);
+
+//     return {
+//       count
+//     };
+//   },
+
+//   components: {}
+// };
+</script>
+
+<style lang="less" scoped></style>
